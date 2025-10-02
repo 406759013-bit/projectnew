78d819c80334c41b7aac8240dc3381865721d79c
rusdee22 <rusdee4027s@gmail.com>
2025-10-03 02:33:11 +0700

    ef

--- app/Enums/UserRole.php
+++ app/Enums/UserRole.php
@@ -18,4 +18,33 @@ enum UserRole: string
             self::MEMBER => 'สมาชิก',
         };
     }
+
+    public function permissions(): array
+    {
+        return match($this) {
+            self::ADMIN => [
+                'manage_users',
+                'manage_managers',
+                'manage_staff',
+                'manage_members',
+                'view_reports',
+                'approve_members',
+                'system_settings'
+            ],
+            self::MANAGER => [
+                'manage_staff',
+                'view_reports',
+                'view_members'
+            ],
+            self::STAFF => [
+                'approve_members',
+                'manage_members',
+                'view_members'
+            ],
+            self::MEMBER => [
+                'view_own_profile',
+                'edit_own_profile'
+            ],
+        };
+    }
 }
